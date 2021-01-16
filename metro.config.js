a6fd38d7842f56ae25a48741e846c508111de681
PetervE <peter@fearless-apps.com>
2021-01-16 12:35:26 +0100

    update metro config

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,8 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+const blacklist = require('metro-config/src/defaults/blacklist');
 module.exports = {
+  resolver: {
+    blacklistRE: blacklist([/#current-cloud-backend\/.*/]),
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
