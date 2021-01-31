5d84534c988ad048bd84835aadd63b4869aca1b1
PetervE <peter@fearless-apps.com>
2021-01-31 16:06:30 +0100

    scrape more info

--- amplify/backend/function/scraper/src/index.js
+++ amplify/backend/function/scraper/src/index.js
@@ -1,5 +1,24 @@
 const chromium = require('chrome-aws-lambda');
 
+async function waitAndClick(index, page) {
+  // await page.waitForFunction(
+  //   `document.querySelector('${selector}') && document.querySelector('${selector}').clientHeight != 0`,
+  //   {visible: true},
+  // );
+
+  const selector = `#divload${index}`;
+  const button = `#click${index}`;
+
+  await page.evaluate(
+    (button) => document.querySelector(button).click(),
+    button,
+  );
+  await page.waitForFunction(
+    `document.querySelector('${selector}') && document.querySelector('${selector}').clientHeight != 0`,
+    {visible: true},
+  );
+}
+
 exports.handler = async (event) => {
   let result = null;
   let browser = null;
@@ -17,13 +36,24 @@ exports.handler = async (event) => {
 
     await page.goto('https://wereldhonden.nl/hond-adopteren');
 
+    for (var i = 0; i < 41; i++) {
+      await waitAndClick(i, page);
+    }
+
     result = await page.evaluate(() => {
       let data = []; // Create an empty array that will store our data
       let elements = document.querySelectorAll('div[itemprop="blogPost"]'); // Select all Products
 
+      let index = 0;
       for (var element of elements) {
-        const title = element.querySelector('h2').innerText;
-        data.push({title}); // Push an object with the data onto our array
+        const title = element.querySelector('h2');
+        const obj = {title: title.innerText};
+
+        const images = element.querySelectorAll('a');
+        if (images.length > 1) obj.image = images[3].getAttribute('href');
+
+        data.push(obj); // Push an object with the data onto our array
+        index + 1;
       }
 
       return data; // Return our data array
@@ -44,7 +74,7 @@ exports.handler = async (event) => {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': '*',
     },
-    body: JSON.stringify(content),
+    body: content,
   };
   return response;
 };
