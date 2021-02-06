61f26ba7182e66bfa1653da05c0e801d475936f1
PetervE <peter@fearless-apps.com>
2021-02-06 17:18:42 +0100

    detail updatee

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -17,7 +17,8 @@ import {
   SafeAreaView,
   AppState,
 } from 'react-native';
-import {Button, Title, Divider} from 'react-native-paper';
+import {Button, Title, Divider, Badge} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import YoutubePlayer from 'react-native-youtube-iframe';
 import {Loader, Centered} from '../components/common';
 import {windowWidth, windowHeight} from '../components/device';
@@ -48,11 +49,10 @@ const Detail = (props) => {
 
   const [playing, setPlaying] = useState(false);
 
+  const [activeMedia, setActiveMedia] = useState('fotos');
+
   const onStateChange = useCallback((state) => {
-    if (state === 'ended') {
-      setPlaying(false);
-      Alert.alert('video has finished playing!');
-    }
+    console.log('state', state);
   }, []);
 
   const togglePlaying = useCallback(() => {
@@ -69,33 +69,31 @@ const Detail = (props) => {
   }, []);
 
   const renderItem = ({item, index}) => {
-    return (
-      <View>
-        <ImageBackground
-          source={{uri: `https://wereldhonden.nl${item}`}}
-          style={{
-            height: 400,
-          }}
-        />
-      </View>
-    );
-  };
-
-  const renderVideo = ({item}) => {
-    let id = item.split('/')[2].split('?')[0];
-    console.log('s1', id);
-
-    return (
-      <View>
-        <YoutubePlayer
-          height={300}
-          play={playing}
-          videoId={id}
-          onChangeState={onStateChange}
-        />
-        <Button title={playing ? 'pause' : 'play'} onPress={togglePlaying} />
-      </View>
-    );
+    if (activeMedia === 'fotos') {
+      return (
+        <View>
+          <ImageBackground
+            source={{uri: `https://wereldhonden.nl${item}`}}
+            style={{
+              height: 400,
+            }}
+          />
+        </View>
+      );
+    }
+    if (activeMedia === 'videos') {
+      return (
+        <View>
+          <YoutubePlayer
+            height={300}
+            play={playing}
+            videoId={id}
+            onChangeState={onStateChange}
+          />
+          <Button title={playing ? 'pause' : 'play'} onPress={togglePlaying} />
+        </View>
+      );
+    }
   };
 
   if (!activeDog) {
@@ -108,24 +106,64 @@ const Detail = (props) => {
 
   return (
     <View style={styles.container}>
-      <Title style={{textAlign: 'center'}}>{activeDog.titel}</Title>
+      <View
+        style={{
+          backgroundColor: 'white',
+          height: 50,
+          marginBottom: 8,
+          alignItems: 'center',
+          justifyContent: 'space-around',
+          flexDirection: 'row',
+        }}>
+        <View>
+          <Title style={{textAlign: 'center'}}>{activeDog.titel}</Title>
+        </View>
+      </View>
+
+      <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+        <Button
+          icon={() => <Icon name="camera" size={18} color="#222222" />}
+          mode="text"
+          style={{
+            marginVertical: 8,
+          }}
+          labelStyle={{
+            color: '#222222',
+            textDecorationLine: activeMedia === 'fotos' ? 'underline' : 'none',
+          }}
+          contentStyle={{
+            height: 50,
+            paddingHorizontal: 12,
+          }}
+          onPress={() => setActiveMedia('fotos')}>
+          Photos
+        </Button>
+        <Button
+          icon={() => <Icon name="video-camera" size={18} color="#222222" />}
+          mode="text"
+          style={{
+            marginVertical: 8,
+          }}
+          labelStyle={{
+            color: '#222222',
+            textDecorationLine: activeMedia === 'videos' ? 'underline' : 'none',
+          }}
+          contentStyle={{
+            height: 50,
+            paddingHorizontal: 12,
+          }}
+          onPress={() => setActiveMedia('videos')}>
+          Videos
+        </Button>
+      </View>
 
       <Carousel
-        layout={'default'}
-        data={activeDog.fotos}
+        layout={'stack'}
+        data={activeDog[activeMedia]}
         sliderWidth={windowWidth}
-        itemWidth={windowWidth - 60}
+        itemWidth={windowWidth - 20}
         renderItem={renderItem}
-        // onSnapToItem={(index) => console.log(index)}
-      />
-
-      <Carousel
-        layout={'default'}
-        data={activeDog.videos}
-        sliderWidth={windowWidth}
-        itemWidth={windowWidth - 60}
-        renderItem={renderVideo}
-        // onSnapToItem={(index) => console.log(index)}
+        onSnapToItem={(index) => console.log('fotos', index)}
       />
     </View>
   );
